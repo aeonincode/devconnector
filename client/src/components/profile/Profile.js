291dff34e6aab5f292298c6cc4252b9bda448020
aeonincode <khashayar.sasannia@hotmail.com>
2020-08-30 13:31:57 +0200

    ProfileTop & ProfileAbout Components

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
+import ProfileTop from "./ProfileTop";
+import ProfileAbout from "./ProfileAbout";
 import { getProfileById } from "../../actions/profile";
 
 const Profile = (
@@ -10,7 +12,7 @@ const Profile = (
 ) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById]);
+  }, [getProfileById, match.params.id]);
 
   return (
     <Fragment>
@@ -23,6 +25,10 @@ const Profile = (
           (<Link to="/edit-profile" className="btn btn-dark">
             Edit Profile
           </Link>)}
+        <div class="profile-grid my-1">
+          <ProfileTop profile={profile} />
+          <ProfileAbout profile={profile} />
+        </div>
       </Fragment>}
     </Fragment>
   );
