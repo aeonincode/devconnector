5d0f6b414b6580bb1314232a3308d073f5525a2e
aeonincode <khashayar.sasannia@hotmail.com>
2020-09-05 09:40:52 +0200

    PostItem component, like and unlike functionalitty, deleting posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
+import PostItem from "./PostItem";
 import { getPosts } from "../../actions/post";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
@@ -8,7 +10,20 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     getPosts();
   }, [getPosts]);
 
-  return <div />;
+  return loading ? <Spinner /> : (
+    <Fragment>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome to the community
+      </p>
+      {/* PostForm */}
+      <div className="posts">
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
