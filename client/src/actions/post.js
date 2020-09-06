7bd6a1091c30a1aa1bcc42a3e3a335edbacf76ec
aeonincode <khashayar.sasannia@hotmail.com>
2020-09-06 13:16:28 +0200

    adding posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -5,6 +5,7 @@ import {
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
+  ADD_POST,
 } from "./types";
 
 // Get posts
@@ -73,6 +74,32 @@ export const deletePost = (id) =>
       });
 
       dispatch(setAlert("Post Removed", "success"));
+    } catch (err) {
+      dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status },
+      });
+    }
+  };
+
+// Add post
+export const addPost = (formData) =>
+  async (dispatch) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.post("/api/posts", formData, config);
+
+      dispatch({
+        type: ADD_POST,
+        payload: res.data,
+      });
+
+      dispatch(setAlert("Post Created", "success"));
     } catch (err) {
       dispatch({
         type: POST_ERROR,
